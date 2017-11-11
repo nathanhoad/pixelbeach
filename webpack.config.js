9b326b3c8d2304757a2bee89f0d53b13330406e1
Jaap Rood <jaap@jaaprood.nl>
2017-11-11 14:05:46 +1000

    Use webpack to generate full static site and inject Phaser through npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
-const Path = require('path');
+const Path = require("path");
+const Config = require("./config");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/game/index.js',
+  entry: "./src/game/index.js",
   output: {
-    path: Path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: 'game.js'
+    path: Path.resolve(__dirname, "dist", "bundle"),
+    publicPath: "/bundle/",
+    filename: "game.js"
   },
   module: {
     rules: [
       {
         test: /\.js/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "../index.html"
+    })
+  ]
 };

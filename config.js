424628996d6a7c0dbbbe55b6597fe93d128f5cda
Jaap Rood <jaap@jaaprood.nl>
2017-11-11 15:01:32 +1000

    Build and render a preview page in production instead of the game

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 const Path = require("path");
 
+const envName = process.env.NODE_ENV;
+const env = {
+	development: !envName || envName === "development",
+	production: envName === "production"
+};
+
 module.exports = {
+	env: env,
+
 	paths: {
 		src: Path.join(__dirname, "src"),
 		dist: Path.join(__dirname, "dist"),

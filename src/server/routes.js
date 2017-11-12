adfe7ad124a56e93426d8688c73c62ad3f796da3
Jaap Rood <jaap@jaaprood.nl>
2017-11-12 11:35:30 +1000

    Add jwt authentication strategy

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,20 +1,30 @@
+const Invariant = require('invariant');
 const ScoresController = require('./controllers/scores');
+const Config = require('./config');
 
 exports.register = (server, options, next) => {
-  server.route([
-    {
-      method: 'GET',
-      path: '/scores',
-      config: ScoresController.getIndex
-    },
-    {
-      method: 'POST',
-      path: '/scores',
-      config: ScoresController.postIndex
-    }
-  ]);
+  server.register([require('hapi-auth-jwt')], err => {
+    Invariant(!err, `Failed loading plugin: ${err}`);
 
-  next();
+    server.auth.strategy('token', 'jwt', {
+      key: Config.auth.key
+    });
+
+    server.route([
+      {
+        method: 'GET',
+        path: '/scores',
+        config: ScoresController.getIndex
+      },
+      {
+        method: 'POST',
+        path: '/scores',
+        config: ScoresController.postIndex
+      }
+    ]);
+
+    next();
+  });
 };
 
 exports.register.attributes = {
